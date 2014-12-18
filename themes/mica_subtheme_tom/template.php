939dca483e5abd23b8f514e4598da26b7aeaecb7
unknown <trpb2@EPIDPC147.mrc-epid.cam.ac.uk>
2014-12-18 16:15:24 +0000

    cosmetic improvements
    Signed-off-by: unknown <trpb2@EPIDPC147.mrc-epid.cam.ac.uk>

--- themes/mica_subtheme_tom/template.php
+++ themes/mica_subtheme_tom/template.php
@@ -7,3 +7,25 @@
 function mica_subtheme_tom_bootstrap_based_theme() {
   return array('mica_subtheme_tom' => TRUE);
 }
+
+function mica_subtheme_tom_menu_link(array $variables) {
+
+dpm('sdfsdf');
+
+  $element = $variables['element'];
+  
+  dpm($element);
+  // $sub_menu = '';
+
+  // $element['#attributes']['class'][] = 'menu-' . $element['#original_link']['mlid'];
+
+  // if ($element['#below']) {
+    // $sub_menu = drupal_render($element['#below']);
+  // }
+  // $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+  // return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+}
+
+function mica_subtheme_tom_menu_link_alter(&$link) {
+dpm($link);  
+}
