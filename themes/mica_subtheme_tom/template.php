efcce736a39da182e9f0a296c96922a845ce82f6
unknown <trpb2@EPIDPC147.mrc-epid.cam.ac.uk>
2015-01-12 13:58:06 +0000

    visual changes, added auto increment populations
    Signed-off-by: unknown <trpb2@EPIDPC147.mrc-epid.cam.ac.uk>

--- themes/mica_subtheme_tom/template.php
+++ themes/mica_subtheme_tom/template.php
@@ -16,21 +16,29 @@ function mica_subtheme_tom_bootstrap_based_theme() {
 
  // function mica_subtheme_tom_menu_link(array $variables) {
 
-
-  // $element = $variables['element'];
+// dpm($variables);
+// dpm("what");
+  // // $element = $variables['element'];
   
-  // $sub_menu = '';
+  // // $sub_menu = '';
 
-  // $element['#attributes']['class'][] = 'menu-' . $element['#original_link']['mlid'];
+  // // $element['#attributes']['class'][] = 'menu-' . $element['#original_link']['mlid'];
 
  
   
-  // if ($element['#below']) {
-    // $sub_menu = drupal_render($element['#below']);
-  // }
-  // $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+  // // if ($element['#below']) {
+    // // $sub_menu = drupal_render($element['#below']);
+  // // }
+  // // $output = l($element['#title'], $element['#href'], $element['#localized_options']);
 
-		// return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+		// // return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
 
 // }
+
+// function mica_subtheme_tom_links__system_main_menu($variables) {
+    // $pid = variable_get('menu_main_links_source', 'main-menu');
+    // $tree = menu_tree($pid);
+// dpm(drupal_render($tree));
+// }
+
 
